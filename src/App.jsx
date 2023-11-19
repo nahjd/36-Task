2b1d4bf7cf4e4a4f57b12acc0806022240b29a77
nahjd <nahidmemmedov711@gmail.com>
2023-11-19 17:46:52 +0400

    AJDAJSDAD

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Userpage from "./components/UserPage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Products from "./components/Products";
+import Layout from "./components/Layout";
+function App() {
+  return (
+    <>
+   
+      <Userpage/>
+    </>
+  );
+}
+
+export default App;
